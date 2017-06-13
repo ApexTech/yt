8b5cc9dfaeb4bd9179e231ef972058c452edfd43
Marcos Mercedes <marcos.mercedesn@gmail.com>
2017-06-13 13:36:56 -0400

    Integrate scheduled fields to snippet

--- lib/yt/models/snippet.rb
+++ lib/yt/models/snippet.rb
@@ -37,6 +37,8 @@ module Yt
       has_attribute :parent_id
       has_attribute :like_count, type: Integer
       has_attribute :updated_at, type: Time
+      has_attribute :scheduled_start_time, type: Time
+      has_attribute :scheduled_end_time, type: Time
 
       def thumbnail_url(size = :default)
         thumbnails.fetch(size.to_s, {})['url']
